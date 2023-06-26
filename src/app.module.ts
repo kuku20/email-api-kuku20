cd04079a9da8ab9d291370a8bfaefa6a6e40ea7c
kuku20 <lluu3790@gmail.com>
2023-06-25 21:49:53 -0500

    This cockDB work

--- src/app.module.ts
+++ src/app.module.ts
@@ -47,7 +47,7 @@ import { SeedService } from './SeedData/shop.service';
             options: routingId,
           },
           entities: [User, Product, ProductBrand, ProductType],
-          synchronize: true,
+          synchronize: false,
         };
       },
     }),
--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,10 @@ async function bootstrap() {
     forbidNonWhitelisted:true
   }))
   
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  app.listen(port, '0.0.0.0', function () {
+    console.log(`Application listening on port ${port}`);
+  });
 }
 
 bootstrap();
