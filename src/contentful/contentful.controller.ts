440dbe8cd5fdf5f04e9eae74197df89916ae947e
loc <lluu3790@gmail.com>
2024-07-04 13:16:59 -0500

    remove profolio endpoint

--- src/contentful/contentful.controller.ts
+++ src/contentful/contentful.controller.ts
@@ -18,17 +18,7 @@ import { ContentfulDto } from './dto/env.dto';
 @Controller('contentful')
 export class ContentfulController {
   constructor(private readonly contentfulService: ContentfulService) {}
-  @Get('/profolio')
-  async profolioData(@Param('preview') preview: boolean) {
-    try {
-      const data = await this.contentfulService.fetchData(preview);
-      return data;
-    } catch (error) {
-      // Handle errors here
-      throw error;
-    }
-  }
-
+  
   @Get('/letcode')
   async leetCodeData(@Param('preview') preview: boolean) {
     try {
