06676675189eb6831a44fe94b3229523da6ffa86
kuku20 <lluu3790@gmail.com>
2024-10-12 20:17:36 -0500

    fix email

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,22 +6,22 @@ export class AppService {
   constructor(private mailerService: MailerService) {}
 
   async sendMail(data: any): Promise<any> {
-    const { date, name, email, message } = data;
+    const { date, name, email, message , host } = data;
+      const hostDetail =host?host: 'Lewis Luu'
     try {
       const result = await this.mailerService.sendMail({
         to: 'loc.luu.1990@gmail.com',
         from: 'emailluu90@gmail.com',
-        subject: 'LOC JOB ON MY WEB',
+        subject: `LOC JOB ON MY WEB + ${hostDetail}`,
         text: `Date: ${date}\nName: ${name}\nEmail: ${email}\nMessage: ${message}`,
         html: `<p>Date: ${date}</p><p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
       });
-
       const reply = await this.mailerService.sendMail({
           to: email,
           from: 'emailluu90@gmail.com',
-          subject: 'LEWIS.LUU-- WEB DEVELOPER',
-          text: `Hello ${name}, \nThank you for sending me a message! I will get back to you as soon as possible.\nBest! \nLewisluu`,
-          html: `<p>Hello ${name}, </p><p>Thank you for sending me a message! I will get back to you as soon as possible.</p><p>Best! </p><p>Lewisluu<p>`,
+          subject: `${hostDetail.toUpperCase()} -- WEB DEVELOPER`,
+          text: `Hello ${name}, \nThank you for sending me a message! I will get back to you as soon as possible.\nBest! \n ${hostDetail}`,
+          html: `<p>Hello ${name}, </p><p>Thank you for sending me a message! I will get back to you as soon as possible.</p><p>Best! </p><p>${hostDetail}<p>`,
       });
       return { message: 'Email sent successfully' };
     } catch (error) {
