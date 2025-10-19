e4543f66a406acda081c76d2ee04a43d67a9217d
loc <lluu3790@gmail.com>
2025-10-19 00:10:19 -0500

    back

--- src/webhooks/webhooks.controller.ts
+++ src/webhooks/webhooks.controller.ts
@@ -1,15 +1,14 @@
-import { Controller, Post, Body, UseGuards, UploadedFile, UseInterceptors, Req } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { WebhooksService } from './webhooks.service';
 import { JwtGuard } from 'src/auth/guard';
 import { AdminUserAuthGuard } from 'src/stock-user/guard';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { Request } from 'express';
-// @UseGuards(JwtGuard)
+
+@UseGuards(JwtGuard)
 @Controller('webhooks')
 export class WebhooksController {
   constructor(private readonly webhooksService: WebhooksService) {}
 
-  @UseGuards(JwtGuard)
   @Post('discord')
   @UseInterceptors(FileInterceptor('file'))
   async sendDiscordNotification(
@@ -30,15 +29,14 @@ export class WebhooksController {
       throw err;
     }
   }
-  
-  @UseGuards(JwtGuard)
+
   @Post('slack')
   async sendSlackNotification(@Body('message') message: string) {
     const result = await this.webhooksService.sendSlackNotification(message);
     return result;
   }
 
-  @UseGuards(JwtGuard)
+
   @UseGuards(AdminUserAuthGuard)
   @Post('delete-messages')
   async deleteMessagesByDay(
@@ -49,28 +47,4 @@ export class WebhooksController {
     return result;
   }
 
-  @Post('stockchartabc')
-  @UseInterceptors(FileInterceptor('file'))
-  async stockchartabc(
-    @UploadedFile() file: import('multer').File,
-    @Body('message') message: string,
-    @Body('botname') botname: string,
-    @Body('lastdata') lastdata: string,
-    @Req() req: Request, // <--- Import from express
-  ) {
-    try {
-      const stockchartabc = `${req.get('host')}`.includes('stock-chart-abc.web.app');
-      console.log(stockchartabc,`${req.get('host')}`)
-      if(!stockchartabc) return null
-      return await this.webhooksService.sendDiscordNotification(
-        message,
-        botname,
-        lastdata,
-        file,
-      );
-    } catch (err) {
-      console.error('❌ Error in controller:', err);
-      throw err;
-    }
-  }
 }
