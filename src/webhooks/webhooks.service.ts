b4326ca755a3ec71ed868ea25c937de3803b2fa5
kuku20 <lluu3790@gmail.com>
2026-03-03 09:43:32 -0600

    not on minute

--- src/webhooks/webhooks.service.ts
+++ src/webhooks/webhooks.service.ts
@@ -1232,7 +1232,7 @@ export class WebhooksService {
         lastdata,
         Secondlastdata,
       );
-    if(timeframe.includes('h') && BuyOnly_StochRSICrossAB200.PriceCrMA50) {
+    if(!timeframe.includes('m') && BuyOnly_StochRSICrossAB200.PriceCrMA50) {
       await this.sendDiscord(
         `SBUY-BuyOnly_StochRSICrossAB200-PriceCrMA50 -${timeframe}(MACD:${lastdata?.MACDLine}): ${lastdata?.date}`,
         `${ticker}-ON-${timeframe}`,
