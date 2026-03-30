23bff577b22a8eb40a72a81e1bedb141ca933e52
loc <lluu3790@gmail.com>
2026-03-30 11:31:31 -0500

    ddd

--- src/stock/task_forex.service.ts
+++ src/stock/task_forex.service.ts
@@ -68,7 +68,7 @@ export class TasksForexService {
       }
     }
   }
- // @Cron('*/15 * * * *') // every 15 minutes
+@Cron('*/15 * * * *') // every 15 minutes
   async handle15minForex() {
     const tickers = ['EURUSD', 'GBPUSD'];
     // const tickers = ['EURUSD'];
