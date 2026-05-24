87c16eb33b39c234b5d2657acacf88f691e07238
loc <lluu3790@gmail.com>
2026-05-23 20:06:07 -0500

    skip 30 min

--- src/stock/task_crypto.service.ts
+++ src/stock/task_crypto.service.ts
@@ -182,7 +182,7 @@ export class TaskCryptoService {
     );
   }
 
-  @Cron(CronExpression.EVERY_30_MINUTES)
+  // @Cron(CronExpression.EVERY_30_MINUTES)
   async handle30pCrypto() {
     // await this.webhooksService.sendDiscord(
     //   'WAKEUPCALL:30min',
