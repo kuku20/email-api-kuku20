91260cbf30d075ebdbcae539d18e52cfa40c06a9
loc <lluu3790@gmail.com>
2026-07-28 16:23:32 -0500

    (!isWithinRange || match)

--- src/stock/task_bull_bear.service.ts
+++ src/stock/task_bull_bear.service.ts
@@ -398,7 +398,7 @@ export class TasksBullBearService {
           );
           const getLastTimePost = this.webhooksService.getTsBySymbol(ticker,this.stockHelperService.lastPosted)
           const match = getLastTimePost ===  last5min?.date
-          if (isWithinRange || match) {
+          if (!isWithinRange || match) {
             await this.webhooksService.sendSlackNotification(`*${ticker}|${last5min.close}|${last5min?.date}* || ${getLastTimePost}`,this.stockHelperService.Z_US_SL_.OR4);
             await this.stockHelperService.sleep(100);
             return 0
