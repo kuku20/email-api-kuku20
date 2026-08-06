e967d795189ec26b295060d9f362a5a15eb2efbc
kuku20 <lluu3790@gmail.com>
2026-08-06 17:35:12 -0500

    kyobe

---
+++ src/task.providers.ts
@@ -0,0 +1,30 @@
+// task.providers.ts
+
+import { Provider } from '@nestjs/common';
+import { TaskCryptoService } from './stock/task_crypto.service';
+import { TasksForexService } from './stock/task_forex.service';
+import { TaskQQQ_SPYService } from './stock/task_QQQ_SPY.service';
+import { Tasks_US_WEEKLY } from './stock/task_us_weekly.service';
+import { TasksUS_ALL_MK_MASS_MACD_OSC } from './stock/task_usmkall_mass_macd_osc.service';
+import { TasksService } from './stock/tasks.service';
+import { ConfigService } from '@nestjs/config';
+import { TasksVNMKService } from './stock/task_vn600.service';
+
+
+export function getTaskProviders(
+    config: ConfigService,
+): Provider[] {
+  const providers: Provider[] = [
+    Tasks_US_WEEKLY,
+    TasksService,
+    TasksUS_ALL_MK_MASS_MACD_OSC,
+    TaskQQQ_SPYService,
+    // TasksVNMKService
+  ];
+  if (config.get<any>( 'TURN_ON1_OFF0') === "1") {
+    providers.push(TaskCryptoService);
+    providers.push(TasksForexService);
+  }
+
+  return providers;
+}
