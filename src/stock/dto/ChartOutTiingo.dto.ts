c79efcf33c4d324c141205233e7a1a9c6d8f47a7
loc <lluu3790@gmail.com>
2026-08-31 22:28:54 -0500

    test me now

--- src/stock/dto/ChartOutTiingo.dto.ts
+++ src/stock/dto/ChartOutTiingo.dto.ts
@@ -1,30 +1,45 @@
 import { Expose, Transform } from 'class-transformer';
 
+const TIME_ZONE = 'America/Chicago';
+
 export class ChartOutTiingo {
-  @Expose({name:'ticker'})
-  ticker: string
+  @Expose({ name: 'ticker' })
+  ticker: string;
 
-  @Expose({name:'open'})
-  open: number
+  @Expose({ name: 'open' })
+  open: number;
 
-  @Expose({name:'close'})
-  close: number
+  @Expose({ name: 'close' })
+  close: number;
 
-  @Expose({name:'low'})
-  low: number
+  @Expose({ name: 'low' })
+  low: number;
 
-  @Expose({name:'high'})
-  high: number
+  @Expose({ name: 'high' })
+  high: number;
 
   @Expose({ name: 'date' })
   @Transform(({ value }) => {
     if (!value) return null;
+
     const date = new Date(value);
-    return date.toLocaleString('en-CA', { hour12: false }).replace(',', '');
+
+    return date
+      .toLocaleString('en-CA', {
+        timeZone: TIME_ZONE,
+        hour12: false,
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+      .replace(',', '');
   })
   date: string;
-
 }
+
 
 // (EST/EDT)
 // @Transform(({ value }) => {
--- src/stock/task_forex.service.ts
+++ src/stock/task_forex.service.ts
@@ -14,7 +14,7 @@ export class TasksForexService {
     private readonly webhooksService: WebhooksService,
   ) {}
   private readonly logger = new Logger(TasksForexService.name);
-
+  tickers = ['EURUSD', 'GBPUSD'];
   private async processTickers1hour(
     tickers: string[],
     timeframe: string,
@@ -68,9 +68,7 @@ export class TasksForexService {
     }
   }
 // @Cron('*/15 * * * *') // every 15 minutes
-  async handle15minForex(time_wait = 3) {
-    const tickers = ['EURUSD', 'GBPUSD'];
-    // const tickers = ['EURUSD'];
+  async handle15minForex(time_wait = 3,tickers = this.tickers) {
     await this.processTickers1hour(
       tickers,
       '15min',
@@ -81,9 +79,7 @@ export class TasksForexService {
     );
   }
   @Cron(CronExpression.EVERY_30_MINUTES)
-  async handle30minForex(time_wait = 3) {
-    const tickers = ['EURUSD', 'GBPUSD'];
-    // const tickers = ['EURUSD'];
+  async handle30minForex(time_wait = 3,tickers = this.tickers) {
     await this.processTickers1hour(
       tickers,
       '30min',
@@ -94,9 +90,7 @@ export class TasksForexService {
     );
   }
   @Cron('0 * * * *') // every 1 hour
-  async handle1hourForex(time_wait = 5) {
-    const tickers = ['EURUSD', 'GBPUSD'];
-    // const tickers = ['EURUSD'];
+  async handle1hourForex(time_wait = 5,tickers = this.tickers) {
     await this.processTickers1hour(
       tickers,
       '1hour',
@@ -107,9 +101,7 @@ export class TasksForexService {
     );
   }
   @Cron(CronExpression.EVERY_4_HOURS)
-  async handle4hourForex(time_wait = 5) {
-    const tickers = ['EURUSD', 'GBPUSD'];
-    // const tickers = ['EURUSD'];
+  async handle4hourForex(time_wait = 5,tickers = this.tickers){
     await this.processTickers1hour(
       tickers,
       '4hour',
@@ -121,7 +113,7 @@ export class TasksForexService {
   }
 
   async onModuleInit() {
-    await this.handle1hourForex(0)
+    await this.handle1hourForex(0,['EURUSD'])
     this.webhooksService.sendDiscord(
       `Run On deploy: **TasksForexService**`,
       `RSIENDBOT ON TasksForexService`,
