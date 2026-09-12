e9d27ef2f59b439830d174630f98e07ce86ebaf0
loc <lluu3790@gmail.com>
2026-09-12 14:44:12 -0500

    isNotSymbol update me for furture

--- src/webhooks/webhooks.service.ts
+++ src/webhooks/webhooks.service.ts
@@ -3752,7 +3752,8 @@ async deleteAllMessages_SLack(channel: string) {
     }
 
     async Post2MySlack(messgage,ticker,channelN=this.sH_Service.DC_SL_MT.ALL_IN_ONE,){
-      let websiteLink =   `|| <${this.sH_Service.local4200}/price-log/${ticker}?daysRange=5|${ticker}-local-target> || <${this.sH_Service.stockMk000}/price-log/${ticker}?daysRange=5|${ticker}-prod-target>`
+      const isNotSymbol = this.sH_Service.isNotSymbol.includes(ticker)
+      let websiteLink =  isNotSymbol?'': `|| <${this.sH_Service.local4200}/price-log/${ticker}?daysRange=5|${ticker}-local-target> || <${this.sH_Service.stockMk000}/price-log/${ticker}?daysRange=5|${ticker}-prod-target>`
       const tsNCh = this.getTsBySymbol(ticker, this.sH_Service.watchlistSl_tss) || this.getTsBySymbol(ticker, this.sH_Service.holdingSl_tss);
       if (tsNCh) {
         const signalThread = this.sH_Service.getSlackThread_MySlack(
