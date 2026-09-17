ce300ec7beb51c04ffe8b640133a8ce66b1a42e4
loc <lluu3790@gmail.com>
2026-09-16 23:56:18 -0500

    fix for OCI

--- src/webhooks/webhooks.service.ts
+++ src/webhooks/webhooks.service.ts
@@ -463,7 +463,13 @@ export class WebhooksService implements OnModuleInit{
     try {
       const browser = await puppeteer.launch({
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        executablePath: '/usr/bin/chromium',
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+          '--disable-gpu',
+        ],
       });
       const page = await browser.newPage();
       // Set the viewport to the full screen size
